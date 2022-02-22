663b2e23b03cb7ac464241ae78e5e964823cc2e5
ayahamane <aya.hamane@epfl.ch>
2022-02-22 09:55:27 +0100

    convertir

--- src/ch/epfl/javelo/projection/Ch1903.java
+++ src/ch/epfl/javelo/projection/Ch1903.java
@@ -1,4 +1,23 @@
 package ch.epfl.javelo.projection;
 
-public class Ch1903 {
+public final class Ch1903 {
+    private Ch1903() {}
+
+    public static double e(double lon, double lat){
+        double lon1 = Math.pow(10,-4)*(3600*Math.toDegrees(lon) - 26782.5);
+        double lat1 = Math.pow(10,-4)*(3600*Math.toDegrees(lat) - 169028.66);
+        double e = 2600072.37+211455.93*lon1 - 10938.51*lon1*lat1-0.36*lon1*lat1*lat1
+                - 44.54*Math.pow(lon1,3);
+        return e;
+    }
+    public static double n(double lon, double lat){
+        
+        return 0;
+    }
+    public static double lon(double e, double n){
+      return 0;
+    }
+    public static double lat(double e, double n){
+        return 0;
+    }
 }
--- src/ch/epfl/javelo/projection/SwissBounds.java
+++ src/ch/epfl/javelo/projection/SwissBounds.java
@@ -1,7 +1,7 @@
 package ch.epfl.javelo.projection;
 
 public final class SwissBounds {
-    private SwissBounds(){}
+    private SwissBounds() {}
 
     final static double MIN_E = 2485000;
     final static double MAX_E = 2834000;
