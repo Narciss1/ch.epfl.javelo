ce99a733feb3e046528bf18aae719dca5d7c7e74
ayahamane <aya.hamane@epfl.ch>
2022-02-24 20:02:30 +0100

    not finished TT

--- src/ch/epfl/javelo/Bits.java
+++ src/ch/epfl/javelo/Bits.java
@@ -1,4 +1,21 @@
 package ch.epfl.javelo;
 
-public class Bits {
+public final class Bits {
+
+    private Bits(){}
+
+    public int extractSigned(int value, int start, int length){
+        if(start>=0 && start<=31 && length>=0 && length<=31){ //A verifier
+            return 0;
+        }else{
+            throw new IllegalArgumentException();
+        }
+    }
+    public int extractUnsigned(int value, int start, int length){
+        if(start>=0 && start<=31 && length>=0 && length<=31){ //A verifier
+            return 0;                                        //pas de size 32 meaning?
+        }else{
+            throw new IllegalArgumentException();
+        }
+    }
 }
---
+++ src/ch/epfl/javelo/Q28_4.java
@@ -0,0 +1,16 @@
+package ch.epfl.javelo;
+
+public final class Q28_4 {
+
+    private Q28_4(){}
+
+    public int ofInt(int i){
+
+    }
+    public double asDouble(int q28_4){
+
+    }
+    public float asFloat(int q28_4){
+        
+    }
+}
