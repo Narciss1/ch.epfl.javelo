267a921c5677798f5d71d781f7a67f7b46c36392
Narciss1 <88516365+Narciss1@users.noreply.github.com>
2022-03-02 17:26:13 +0100

    Lina_Etape2(sans erreurs normally)2

--- src/ch/epfl/javelo/Functions.java
+++ src/ch/epfl/javelo/Functions.java
@@ -7,10 +7,22 @@ public final class Functions
 
     private Functions (){ }
 
+    /**
+     *
+     * @param y the image we want the constant function to give
+     * @return a new Object of type Constant
+     */
     public static DoubleUnaryOperator constant(double y){
         return new Constant(y);
     }
 
+
+    /**
+     *
+     * @param samples an array that contains the images of some values
+     * @param xMax the last antecedent for which we have the image in samples
+     * @return
+     */
     public static DoubleUnaryOperator sampled(float[] samples, double xMax){
         Preconditions.checkArgument(samples.length >= 2);
         Preconditions.checkArgument(xMax > 0);
@@ -21,6 +33,11 @@ public final class Functions
     private static record Constant(double constantValue) implements DoubleUnaryOperator {
 
 
+        /**
+         *
+         * @param x the antecedent
+         * @return the image of x
+         */
         @Override
         public double applyAsDouble(double x){
             return constantValue;
@@ -38,22 +55,27 @@ public final class Functions
             this.xMax = xMax;
         }
 
+        /**
+         *
+         * @param x the antecedent
+         * @return the image of x
+         */
         @Override
         public double applyAsDouble(double x){
+            //The gap is the distance between two antecents which image is in the array samples
             double gap = xMax / (samples.length - 1);
             double floorValue = Math.floor(x / gap);
             if (x > 0 && x < xMax) {
                 return Math2.interpolate(samples[(int)(floorValue)],
-                        samples[(int)(floorValue)+1] ,
-                        (x - floorValue* gap) / gap );
+                        samples[(int)(floorValue) + 1] ,
+                        (x - floorValue * gap) / gap );
             }
             else if (x <= 0){
                 return samples[0];
             }
-            return samples[samples.length-1];
+            return samples[samples.length - 1];
         }
 
     }
-
 
 }
--- src/ch/epfl/javelo/data/AttributeSet.java
+++ src/ch/epfl/javelo/data/AttributeSet.java
@@ -10,21 +10,36 @@ import static ch.epfl.javelo.data.Attribute.COUNT;
 public record AttributeSet (long bits) {
 
 
+
     public AttributeSet {
         long testBits = bits >>> COUNT;
         Preconditions.checkArgument(testBits == 0L);
     }
 
+    /**
+     *
+     * @param attribute we want to check if it is the set
+     * @return true if attribute is in the set
+     */
     public boolean contains(Attribute attribute){
         long attributeMask = 1L << attribute.ordinal();
         long testMask = attributeMask & bits;
         return (testMask != 0L);
     }
 
+    /**
+     *
+     * @param that another set of attributes
+     * @return true if the two sets share at least one attribute
+     */
     public boolean intersects(AttributeSet that){
         return ((bits & that.bits) != 0L);
     }
 
+    /**
+     *
+     * @return the name of all the attributes in the set
+     */
     @Override
     public String toString(){
         String toReturn = "{";
@@ -33,7 +48,7 @@ public record AttributeSet (long bits) {
             if (contains(ALL.get(i))){
                 if (!starting)
                     toReturn = toReturn + "," + ALL.get(i).toString();
-                if (starting){
+                else {
                     toReturn = toReturn + ALL.get(i).toString();
                     starting = false;
                 }
@@ -45,6 +60,11 @@ public record AttributeSet (long bits) {
     }
 
 
+    /**
+     *
+     * @param attributes list of attributes we want in the set
+     * @return a new set containing the attributes given
+     */
     public static AttributeSet of (Attribute... attributes){
         long bitsIncremented = 0L;
         for (int i = 0; i < attributes.length; ++i){
