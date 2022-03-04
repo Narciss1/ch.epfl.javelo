8a0a2485cea8009f3d76ff0f8052031ccfba5eef
ayahamane <aya.hamane@epfl.ch>
2022-03-04 14:56:08 +0100

    GraphSectors done sans test

--- src/ch/epfl/javelo/data/GraphSectors.java
+++ src/ch/epfl/javelo/data/GraphSectors.java
@@ -12,43 +12,43 @@ import static javax.swing.UIManager.getInt;
 
 public record GraphSectors(ByteBuffer buffer) {
 
-    //0)Est-ce qu'on peut use simplement une collision physique entre carré et rectangles?
-    //1)Est-ce que les constantes évoluent en unité de bits ou vraiment en octet 8 par 8?
     private static final int OFFSET_Index = 0;
     private static final int OFFSET_Number = OFFSET_Index + Integer.BYTES;
     private static final int SECTOR_INTS = OFFSET_Number + Short.BYTES;
 
     public List<Sector> sectorsInArea(PointCh center, double distance) {
+
         ArrayList<Sector> sectorInArea = new ArrayList<Sector>();
+
         PointCh leftDown = new PointCh(center.getE() - distance, center.getN() - distance);
         PointCh rightDown = new PointCh(center.getE() + distance, center.getN() - distance);
         PointCh leftUp = new PointCh(center.getE() - distance, center.getN() + distance);
         PointCh rightUp = new PointCh(center.getE() + distance, center.getN() + distance);
+
         int xMin = (int) (leftDown.getE()/2730.0);
         int xMax = (int) (rightDown.getE()/2730.0);
         int yMin = (int) (leftDown.getN()/1730.0);
-        int yMax = (int) (leftUp.getN()/1730.0);
+        int yMax = (int) (rightUp.getN()/1730.0);
+
         int indexLeftDown = xMin + 128*yMin;
         int indexLeftUp = xMin + 128*yMax;
         int indexRightDown = xMax + 128*yMin;
         int indexRightUp = xMax + 128*yMax;
 
+        int largeur = indexRightDown - indexLeftDown + 1;
+
+        for(int i = indexLeftDown; i <= indexRightUp; i += 128){
+            for(int j = 0; j <= largeur - 1; ++j){
+                int indexStartNode = buffer.getInt((i + j)*SECTOR_INTS + OFFSET_Index);
+                int numberNodes = toUnsignedInt(buffer().getShort((i + j)*SECTOR_INTS + OFFSET_Number));
+                int indexEndNode = indexStartNode + numberNodes - 1;
+                sectorInArea.add(new Sector(indexStartNode, indexEndNode));
+            }
+        }
         return sectorInArea;
     }
 
-
-
-
-   /* for (int i = 0; i < buffer.capacity(); i++) {
-        //3)Est-ce que ces deux variables sont correctes?
-        int index = buffer.getInt(buffer.get(i) + OFFSET_Index);
-        int number = toUnsignedInt(buffer().getShort(buffer.get(i) + OFFSET_Number));
-        //4)Est-ce qu'on peut prendre les startNodeId et endNodeId directement? Ou est-ce qu'on doit définir ces méthodes dans le record?
-        //5)Choisir de comparer leur e et n aux limites définies via des checkArgument() est une bonne idée?
-        //6)Comment on peut ajouter au tableau des Sector?
-    }*/
-    //7)Quel droit d'accès?
-    //8)Laisser vide ou censés add things?
     record Sector(int startNodeId, int endNodeId) {
+
     }
 }
