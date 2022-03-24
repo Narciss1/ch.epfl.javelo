1ced353250c44fec74d594d297f65e93de66908c
Narciss1 <88516365+Narciss1@users.noreply.github.com>
2022-03-24 17:37:15 +0100

    Lina_Etape6_debut

--- src/ch/epfl/javelo/routing/RouteComputer.java
+++ src/ch/epfl/javelo/routing/RouteComputer.java
@@ -3,6 +3,10 @@ package ch.epfl.javelo.routing;
 import ch.epfl.javelo.Preconditions;
 import ch.epfl.javelo.data.Graph;
 
+import java.util.HashSet;
+
+import static java.util.Arrays.fill;
+
 public final class RouteComputer {
 
     private final Graph graph;
@@ -15,9 +19,43 @@ public final class RouteComputer {
         this.costFunction = costFunction;
     }
 
-//    public Route bestRouteBetween(int startNodeId, int endNodeId){
-//        Preconditions.checkArgument(startNodeId != endNodeId);
-//
-//    }
+    public Route bestRouteBetween(int startNodeId, int endNodeId) {
+        Preconditions.checkArgument(startNodeId != endNodeId);
+        double[] distances = new double[graph.nodeCount()];
+        distances[0] = 0;
+        double[] predecessors = new double[graph.nodeCount()];
+        fill(distances, 1, distances.length, Double.POSITIVE_INFINITY);
+        fill(predecessors, 0, predecessors.length, -1);
+        HashSet<Integer> explorating = new HashSet<>();
+        explorating.add(startNodeId);
+        while (!explorating.isEmpty()) {
+            int currentNode = currentNode(distances, explorating);
+            if (currentNode == endNodeId) {
+                //Faut retourner la Route avec les bonnes edges. (Piazza) ToDO.
+            } else { //else à tej vu qu'on mettre return plus haut.
+                for (int i = 0; i < graph.nodeOutDegree(currentNode); ++i){
+                    int targetNode = graph.edgeTargetNodeId(graph.nodeOutEdgeId(currentNode, i));
+                    double potentialDistance = graph.edgeLength(graph.nodeOutEdgeId(currentNode, i));
+                    if (potentialDistance < distances[targetNode]){
+                        distances[targetNode] = potentialDistance;
+                        predecessors[targetNode] = currentNode;
+                        explorating.add(targetNode);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private int currentNode (double[] distances, HashSet<Integer> explorating){
+        double distanceToKeep = Double.POSITIVE_INFINITY;
+        int nodeToKeep = 0;
+        for (Integer node : explorating){
+            if (distances[node] < distanceToKeep){
+                nodeToKeep = node;
+            }
+        }
+        return nodeToKeep;
+    }
 
 }
--- test/ch/epfl/javelo/routing/ElevationProfileComputerTest.java
+++ test/ch/epfl/javelo/routing/ElevationProfileComputerTest.java
@@ -3,6 +3,7 @@ package ch.epfl.javelo.routing;
 import ch.epfl.javelo.Functions;
 import ch.epfl.javelo.projection.PointCh;
 import ch.epfl.test.TestRandomizer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -251,5 +252,25 @@ public class ElevationProfileComputerTest {
         float[] samplesExamples = {5,NaN, NaN,NaN, 6};
         float[] expected = {5, 5.25f, 5.5f, 5.75f, 6};
         assertArrayEquals(expected, interpolateElevation(samplesExamples));
+    }
+
+    @Test
+    void elevationProfileOneEdgeNotNa(){
+        Edge edge = new Edge(0, 0, null, null, 24,
+                Functions.sampled(new float[]{Float.NaN, 4, 0, Float.NaN, 8, 8, Float.NaN}, 24));
+        List<Edge> edges = new ArrayList<>();
+        edges.add(edge);
+        RouteTest1 routeTest1 = new RouteTest1(edges);
+        System.out.println(routeTest1.elevationAt(8));
+        ElevationProfile elevationProfileTesting = elevationProfile(routeTest1, 4);
+        Assertions.assertEquals(4, elevationProfileTesting.elevationAt(0));
+        Assertions.assertEquals(4, elevationProfileTesting.elevationAt(2));
+        Assertions.assertEquals(2, elevationProfileTesting.elevationAt(6));
+        Assertions.assertEquals(6, elevationProfileTesting.elevationAt(14));
+        Assertions.assertEquals(8, elevationProfileTesting.elevationAt(16));
+        Assertions.assertEquals(8, elevationProfileTesting.elevationAt(21));
+        Assertions.assertEquals(8, elevationProfileTesting.elevationAt(24));
+        Assertions.assertEquals(8, elevationProfileTesting.elevationAt(25));
+        Assertions.assertEquals(4, elevationProfileTesting.elevationAt(-1));
     }
 }
