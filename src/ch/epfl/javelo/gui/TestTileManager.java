59e4e38e32375f906edbfb7528cf37e6f2bef2e9
Narciss1 <88516365+Narciss1@users.noreply.github.com>
2022-04-15 15:49:45 +0200

    Lina_Etape7

--- src/ch/epfl/javelo/gui/TestTileManager.java
+++ src/ch/epfl/javelo/gui/TestTileManager.java
@@ -16,15 +16,17 @@ import java.nio.file.Path;
         public void start(Stage primaryStage) throws Exception {
             TileManager tm = new TileManager(
                     Path.of("."), "tile.openstreetmap.org");
-            for (int i = 1; i <= 103; ++i){
+            for (int i = 1; i <= 6; ++i){
                 Image tileImage = tm.imageForTileAt(
                         new TileManager.TileId(12, i, i));
             }
-//            Image tileImage = tm.imageForTileAt(
+            //Image tileImage = tm.imageForTileAt(
 //                    new TileManager.TileId(17, 10, 20));
-//                    new TileManager.TileId(19, 271720, 185430);
+                   //new TileManager.TileId(19, 271720, 185430));
             Platform.exit();
         }
+
+
     }
 
 
