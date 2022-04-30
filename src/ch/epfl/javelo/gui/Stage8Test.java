629591cd8ab6501265361d88d0d354282707c8b7
Narciss1 <88516365+Narciss1@users.noreply.github.com>
2022-04-30 21:47:55 +0200

    Lina_Etape9_HopefullyFinie

--- src/ch/epfl/javelo/gui/Stage8Test.java
+++ src/ch/epfl/javelo/gui/Stage8Test.java
@@ -11,6 +11,8 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.StackPane;
 import java.nio.file.Path;
 import javafx.stage.Stage;
@@ -76,6 +78,9 @@ public final class Stage8Test extends Application {
         primaryStage.setMinWidth(600);
         primaryStage.setMinHeight(300);
         primaryStage.setScene(new Scene(mainPane));
+        primaryStage.setFullScreen(true);
+        primaryStage.setFullScreenExitHint("Press echap to exit full screen ta race");
+        primaryStage.setFullScreenExitKeyCombination(KeyCombination.valueOf(KeyCode.ESCAPE.getName()));
         primaryStage.show();
     }
 
