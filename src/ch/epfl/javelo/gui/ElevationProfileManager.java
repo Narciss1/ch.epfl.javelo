5888d5e686b8296991913b520f40086ec917f542
ayahamane <aya.hamane@epfl.ch>
2022-05-05 22:50:24 +0200

    Grille

--- src/ch/epfl/javelo/gui/ElevationProfileManager.java
+++ src/ch/epfl/javelo/gui/ElevationProfileManager.java
@@ -16,14 +16,13 @@ import javafx.scene.shape.Polygon;
 import javafx.scene.transform.Affine;
 import javafx.scene.transform.NonInvertibleTransformException;
 import javafx.scene.transform.Transform;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public final class ElevationProfileManager {
 
-    private ReadOnlyObjectProperty<ElevationProfile> elevationProfileProperty; //null
-    private ReadOnlyDoubleProperty highlightedPositionProperty; //nan
+    private ReadOnlyObjectProperty<ElevationProfile> elevationProfileProperty; //null to add
+    private ReadOnlyDoubleProperty highlightedPositionProperty; //nan to add
     private BorderPane borderPane;
     private Pane pane;
     private VBox routeProperties;
@@ -31,9 +30,8 @@ public final class ElevationProfileManager {
     private Insets insets;
     private ObjectProperty<Transform> worldToScreen;
     private ObjectProperty<Transform> screenToWorld;
-    private double xUnit;
-    //private double yUnit;
 
+    //Question : rectangle pas bien dimensionné au départ. Chelou.
     public ElevationProfileManager(ReadOnlyObjectProperty<ElevationProfile> elevationProfileProperty,
                                    ReadOnlyDoubleProperty highlightedPositionProperty) {
         this.elevationProfileProperty = elevationProfileProperty;
@@ -44,35 +42,16 @@ public final class ElevationProfileManager {
         worldToScreen = new SimpleObjectProperty<>();
         pane = new Pane();
         routeProperties = new VBox();
-        routeProperties.setPrefHeight(100);
         borderPane = new BorderPane();
+        routeProperties.setId("profile_data");
         borderPane.getStylesheets().add("elevation_profile.css");
         borderPane.setCenter(pane);
-        //borderPane.setBottom(routeProperties);
+        borderPane.setBottom(routeProperties);
 
+        //IMPORTANT: ADD LA TECHNIQUE STARTY AND ENDY ASSISTANT
         pane.widthProperty().addListener(l -> bindRectangleProperty());
         pane.heightProperty().addListener(l -> bindRectangleProperty());
-
-        //Il faut utiliser des bindings ici et non des listener pour que le rectangle bleu soit toujours
-        // redimensionné en fonction du borderPane
-        rectangleProperty.bind(Bindings.createObjectBinding((() -> {
-            System.out.println("dans bind");
-            if(pane.getWidth() > 0 && pane.getHeight() > 0) {
-                return new Rectangle2D(
-                        insets.getLeft(),
-                        insets.getTop(),
-                        pane.getWidth() - insets.getRight() - insets.getLeft(),
-                        pane.getHeight() - insets.getTop() - insets.getBottom());
-            } else {
-                return new Rectangle2D(insets.getLeft(), insets.getTop(),
-                        0,0);
-            }
-        })));
-
-        //Succession d'appels comme expliqué dans le post piazza que j'ai send sur le groupe
         rectangleProperty.addListener(l -> {
-            System.out.println("listener de rectangle property");
-            System.out.println(rectangleProperty.get());
             transformations();});
         worldToScreen.addListener(l -> createPolygone());
         screenToWorld.addListener(l -> createPolygone());
@@ -89,7 +68,6 @@ public final class ElevationProfileManager {
 
     private void bindRectangleProperty() {
         rectangleProperty.bind(Bindings.createObjectBinding((() -> {
-            System.out.println("dans bind");
             if(pane.getWidth() > 0 && pane.getHeight() > 0) {
                 return new Rectangle2D(
                         insets.getLeft(),
@@ -106,26 +84,14 @@ public final class ElevationProfileManager {
     private void transformations() {
         Affine affine = new Affine();
         ElevationProfile elevationProfile = elevationProfileProperty.get();
-        //Première translation qui permet de passer du pane à l'intérieur du rectangle bleu
         affine.prependTranslation(-insets.getLeft(), -insets.getTop());
-        //2e translation pr faire en sorte que la elevation minimale apparaisse sur le bas de l'écran
-        //mm si elle est de 300 et qlq
-        //affine.prependTranslation(0, elevationProfile.minElevation() - elevationProfile.maxElevation());
-        //Scaling par rapport à la taille du rectangle bleu (le "-" avant celui du y permet d'appliquer directly
-        //la deuxième translation sur la valeur donnée par le scaling)
         affine.prependScale(
                 elevationProfile.length() / rectangleProperty.get().getWidth(),
                 (elevationProfile.minElevation() - elevationProfile.maxElevation()) / rectangleProperty.get().getHeight());
-        System.out.println();
-        Point2D test = new Point2D(
-                elevationProfile.length(),
-                elevationProfile.elevationAt(elevationProfile.minElevation()));
-        //Deuxième translation permettant d'inverser le y
-        affine.prependTranslation(0, rectangleProperty.get().getHeight());
+        affine.prependTranslation(0, elevationProfile.maxElevation());
         screenToWorld.setValue(affine);
         try {
             worldToScreen.setValue(screenToWorld.get().createInverse());
-            System.out.println(worldToScreen.get().transform(test));
         } catch (NonInvertibleTransformException exception) {
             throw new Error (exception);
         }
@@ -134,54 +100,24 @@ public final class ElevationProfileManager {
     private void createPolygone() {
         List<Double> listPoints = new ArrayList<>();
         Rectangle2D rectangle = rectangleProperty.get();
-        double height = rectangle.getHeight();
-        double width = rectangle.getWidth();
-        double minX = rectangle.getMinX();
-        double minY = rectangle.getMinY();
-        double maxX = rectangle.getMaxX();
-        double maxY = rectangle.getMaxY();
-        //4 points du rectangle pour voir déjà/debugger si son affichage se fait bien comme il faut
-        /*listPoints.add(minX);
-        listPoints.add(minY + height);
-        listPoints.add(minX);
-        listPoints.add(minY);
-        listPoints.add(minX + width);
-        listPoints.add(minY);
-        listPoints.add(maxX);
-        listPoints.add(maxY);*/
         ElevationProfile elevationProfile = elevationProfileProperty.get();
-        double positionP = 0;
-        boolean first = true;
+        double positionP = insets.getLeft();
         if(worldToScreen.get() != null) {
-//
+            listPoints.add(insets.getLeft());
+            listPoints.add(insets.getTop() + rectangle.getHeight());
             while (positionP <= rectangleProperty.get().getWidth() + insets.getLeft()) {
-                //Ces deux variables permettent de convertir la position des pxels aux mètres pour use cette value
-                //dans le pointToTransform
-                //Le 0 est une valeur par défaut mais inévitable pour le passage d'un système de coordo à un autre
                 Point2D pointP = new Point2D(positionP, 0);
                 Point2D pointM = screenToWorld.get().transform(pointP);
                 Point2D pointToTransform = new Point2D(pointM.getX(), elevationProfile.elevationAt(pointM.getX()));
                 Point2D pointToAdd = worldToScreen.get().transform(pointToTransform);
-                if (first) {
-                    listPoints.add(pointToAdd.getX());  //il faut ajouter le même x que le premier point
-                    //pour créer une droite entre les deux; (même logique pour le dernier point dans l'autre if).
-                    //Les boucles if servent à vérifier si l'on est au début/ou à la fin pr les add. (c moche oui)
-                    listPoints.add(minY + height);
-                    first = false;
-                }
+
                 listPoints.add(pointToAdd.getX());
                 listPoints.add(pointToAdd.getY());
-                //Il ne faut incrémenter que d'une seule unité (un pixel)
-                ++positionP;
-                if (positionP > rectangleProperty.get().getWidth() + insets.getLeft()) {
-                    listPoints.add(pointToAdd.getX());
-                    listPoints.add(maxY);
-                }
+                positionP += 1;
             }
+            listPoints.add(insets.getLeft() + rectangle.getWidth());
+            listPoints.add(insets.getTop() + rectangle.getHeight());
         }
-        //Valeurs récupérées directement à partir du rectangle
-//        listPoints.add(maxX);
-//        listPoints.add(maxY);
         Polygon profile = new Polygon();
         profile.setId("profile");
         profile.getPoints().addAll(listPoints);
