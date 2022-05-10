795b25abecb7f9085d85b87977fa1d66a77e49b3
ayahamane <aya.hamane@epfl.ch>
2022-05-10 09:25:16 +0200

    ETAPE 10 HOPEFULLY DONE SA RACE

--- src/ch/epfl/javelo/gui/RouteBean.java
+++ src/ch/epfl/javelo/gui/RouteBean.java
@@ -34,7 +34,9 @@ public final class RouteBean {
         waypoints.addListener((InvalidationListener) l -> computingItineraryAndProfile());
     }
 
+
     public void setWaypoints(ObservableList<Waypoint> listOfWaypoints) {
+        //Est-ce que setAll suffisant?
         waypoints.setAll(listOfWaypoints);
     }
 
