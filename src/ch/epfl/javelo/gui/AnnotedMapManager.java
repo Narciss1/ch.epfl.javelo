a27e7a8b08a532be7a5725025ddcb1c871829b20
ayahamane <aya.hamane@epfl.ch>
2022-05-10 23:18:59 +0200

    ETAPE 11

--- src/ch/epfl/javelo/gui/AnnotedMapManager.java
+++ src/ch/epfl/javelo/gui/AnnotedMapManager.java
@@ -1,8 +1,13 @@
 package ch.epfl.javelo.gui;
 
+import ch.epfl.javelo.Math2;
 import ch.epfl.javelo.data.Graph;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
+import ch.epfl.javelo.projection.PointCh;
+import ch.epfl.javelo.projection.PointWebMercator;
+import ch.epfl.javelo.routing.RoutePoint;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
+import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
@@ -15,15 +20,22 @@ public final class AnnotedMapManager {
     private final RouteManager routeManager;
     private final WaypointsManager waypointsManager;
     private final StackPane javeloPane;
-
+    private final RouteBean routeBean;
     private static final MapViewParameters startMapViewParameters =
             new MapViewParameters(12, 543200, 370650);
+    private final ObjectProperty<MapViewParameters> mapViewParametersP;
+    private final ObjectProperty<Point2D> mousePositionP;
+    private final DoubleProperty mousePositionOnRouteProperty;
+
+
 
 
     public AnnotedMapManager(Graph graph, TileManager tileManager, RouteBean routeBean,
                              Consumer<String> errorConsumer) {
-        ObjectProperty<MapViewParameters> mapViewParametersP =
-                new SimpleObjectProperty<>(startMapViewParameters);
+        mapViewParametersP = new SimpleObjectProperty<>(startMapViewParameters);
+        mousePositionP = new SimpleObjectProperty<>();
+        mousePositionOnRouteProperty = new SimpleDoubleProperty();
+        this.routeBean = routeBean;
         waypointsManager = new WaypointsManager(graph, mapViewParametersP, routeBean.waypoints(), errorConsumer);
         baseMapManager = new BaseMapManager(tileManager, waypointsManager, mapViewParametersP);
         //est-ce que c la classe qui se charge de la rendre readOnly?
@@ -31,12 +43,40 @@ public final class AnnotedMapManager {
         javeloPane = new StackPane(baseMapManager.pane(), routeManager.pane(), waypointsManager.pane());
         javeloPane.getStylesheets().add("map.css");
 
-
     }
 
     public Pane pane() {
         return javeloPane;
     }
 
+    public ReadOnlyDoubleProperty mousePositionOnRouteProperty() {
+        return null;
+    }
 
+    private void mousePositionOnRouteBinding() {
+        mousePositionOnRouteProperty.bind(Bindings.createDoubleBinding((() -> {
+            if(mousePositionP.get() == null) {
+                return Double.NaN;
+            }
+            PointWebMercator webMercatorMouse = mapViewParametersP.get().pointAt(
+                    mousePositionP.get().getX(),
+                    mousePositionP.get().getY());
+            PointCh pointChMouse = webMercatorMouse.toPointCh();
+            RoutePoint routePointMouse = routeBean.route().pointClosestTo(pointChMouse);
+            PointWebMercator closestPoint = PointWebMercator.ofPointCh(routePointMouse.point());
+            double distance = Math2.norm(
+                    mapViewParametersP.get().viewX(closestPoint) - mapViewParametersP.get().viewX(webMercatorMouse),
+                    mapViewParametersP.get().viewY(closestPoint) - mapViewParametersP.get().viewY(webMercatorMouse));
+            if (distance <= 15) {
+                return routePointMouse.position();
+            }
+            return Double.NaN;
+        }), mousePositionP, mapViewParametersP, routeBean.routeProperty()));
+    }
+
+    private void setMousePositionP() {
+        baseMapManager.pane().setOnMouseMoved(e ->  mousePositionP.setValue(new Point2D(e.getX(), e.getY())));
+        baseMapManager.pane().setOnMouseExited(e -> mousePositionP.set(null));
+        //Est-ce que détection legit?
+    }
 }
