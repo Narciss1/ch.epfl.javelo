19ddaa88f9980bf78e2f0145327baaa6a3c8cbd2
ayahamane <aya.hamane@epfl.ch>
2022-05-22 22:51:32 +0200

    Etape 11 relecture

--- src/ch/epfl/javelo/gui/AnnotedMapManager.java
+++ src/ch/epfl/javelo/gui/AnnotedMapManager.java
@@ -12,11 +12,17 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import java.util.function.Consumer;
 
+/**
+ * Manages the display of the "annotated" map, i.e. the background map over which
+ * the route and waypoints are superimposed
+ * @author Hamane Aya (345565)
+ * @author Sadgal Lina (342075)
+ */
 public final class AnnotedMapManager {
 
-    private BaseMapManager baseMapManager;
-    private final RouteManager routeManager;
-    private final WaypointsManager waypointsManager;
+    private final BaseMapManager baseMapManager;
+    //private final RouteManager routeManager;
+    //private final WaypointsManager waypointsManager;
 
     private final ObjectProperty<MapViewParameters> mapViewParametersP;
     private final ObjectProperty<Point2D> mousePositionP;
@@ -24,38 +30,67 @@ public final class AnnotedMapManager {
 
     private final StackPane javeloPane;
     private final RouteBean routeBean;
+
+    /**
+     * The parameters of the map at the start
+     */
     private static final MapViewParameters startMapViewParameters =
             new MapViewParameters(12, 543200, 370650);
 
+    /**
+     * Constructor
+     * @param graph a road network graph
+     * @param tileManager an OpenStreetMap tile manager
+     * @param routeBean a route bean
+     * @param errorConsumer an "error consumer" to report an error
+     */
+    //est-ce que c la classe qui se charge de la rendre readOnly? //manager
     public AnnotedMapManager(Graph graph, TileManager tileManager, RouteBean routeBean,
                              Consumer<String> errorConsumer) {
         mapViewParametersP = new SimpleObjectProperty<>(startMapViewParameters);
         mousePositionP = new SimpleObjectProperty<>();
         mousePositionOnRouteProperty = new SimpleDoubleProperty();
 
-        this.routeBean = routeBean;
-        waypointsManager = new WaypointsManager(graph, mapViewParametersP, routeBean.waypoints(), errorConsumer);
+        WaypointsManager waypointsManager = new WaypointsManager(graph, mapViewParametersP, routeBean.waypoints(), errorConsumer);
+        RouteManager routeManager = new RouteManager(routeBean, mapViewParametersP);
         baseMapManager = new BaseMapManager(tileManager, waypointsManager, mapViewParametersP);
-        //est-ce que c la classe qui se charge de la rendre readOnly?
-        routeManager = new RouteManager(routeBean, mapViewParametersP);
+
         javeloPane = new StackPane(baseMapManager.pane(), routeManager.pane(), waypointsManager.pane());
         javeloPane.getStylesheets().add("map.css");
+
+        this.routeBean = routeBean;
         setMousePositionP();
         mousePositionOnRouteBinding();
     }
 
+    /**
+     * Returns the panel containing the annotated map
+     * @return a panel containing the annotated map
+     */
     public Pane pane() {
         return javeloPane;
     }
 
+    /**
+     * Returns the property containing the position of the mouse pointer along the route
+     * @return a property containing the position of the mouse pointer
+     */
     public ReadOnlyDoubleProperty mousePositionOnRouteProperty() {
         return mousePositionOnRouteProperty;
     }
 
+    //Extension
+    /**
+     * Sets the tile manager of the base map manager
+     * @param tileManager a new tile manager
+     */
     public void setTileManager(TileManager tileManager) {
         baseMapManager.setTileManager(tileManager);
     }
-
+    /**
+     * Binds the property containing the mouse position on the route and other
+     * properties containing the map view parameters, the mouse position and the route
+     */
     private void mousePositionOnRouteBinding() {
         mousePositionOnRouteProperty.bind(Bindings.createDoubleBinding((() -> {
             if(mousePositionP.get() == null) {
@@ -66,11 +101,14 @@ public final class AnnotedMapManager {
                     mousePositionP.get().getY());
             PointCh pointChMouse = webMercatorMouse.toPointCh();
             if (routeBean.route() != null && pointChMouse != null) {
-                RoutePoint routePointMouse = routeBean.route().pointClosestTo(pointChMouse);
+                RoutePoint routePointMouse = routeBean.route()
+                                                      .pointClosestTo(pointChMouse);
                 PointWebMercator closestPoint = PointWebMercator.ofPointCh(routePointMouse.point());
                 double distance = Math2.norm(
-                        mapViewParametersP.get().viewX(closestPoint) - mapViewParametersP.get().viewX(webMercatorMouse),
-                        mapViewParametersP.get().viewY(closestPoint) - mapViewParametersP.get().viewY(webMercatorMouse));
+                        mapViewParametersP.get().viewX(closestPoint)
+                                - mapViewParametersP.get().viewX(webMercatorMouse),
+                        mapViewParametersP.get().viewY(closestPoint)
+                                - mapViewParametersP.get().viewY(webMercatorMouse));
                 if (distance <= 15) {
                     return routePointMouse.position();
                 }
@@ -79,15 +117,13 @@ public final class AnnotedMapManager {
         }), mousePositionP, mapViewParametersP, routeBean.routeProperty()));
     }
 
+    /**
+     * Manages the events related to the mouse position on the annotated map
+     */
     private void setMousePositionP() {
-        javeloPane.setOnMouseMoved(e ->  {
-            mousePositionP.setValue(new Point2D(e.getX(), e.getY()));
-        });
+        javeloPane.setOnMouseMoved(e -> mousePositionP.setValue(new Point2D(e.getX(), e.getY())));
 
-        javeloPane.setOnMouseExited(e -> {
-            //Est-ce que détection legit? genre par le nul
-            mousePositionP.set(null);
-        });
+        javeloPane.setOnMouseExited(e -> mousePositionP.set(null));
         //Est-ce que détection legit? genre par le null
     }
 }
