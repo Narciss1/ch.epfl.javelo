ce0d2f4f9f99029404a4a099d91a8cf3a3bf41b2
ayahamane <aya.hamane@epfl.ch>
2022-06-02 10:10:44 +0200

    ETAPE 10 HOPEFULLY DONE SA RACE

--- src/ch/epfl/javelo/gui/SwitchBackgroundManager.java
+++ src/ch/epfl/javelo/gui/SwitchBackgroundManager.java
@@ -2,6 +2,7 @@ package ch.epfl.javelo.gui;
 
 import javafx.beans.binding.Bindings;
 import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
@@ -29,8 +30,7 @@ public final class SwitchBackgroundManager {
     private final static String DEFAULT_LABEL_TEXT = "Fond de carte :";
     private final static String OPEN_STREET_MAP = "OpenStreetMap";
     private final static String OPEN_STREET_MAP_SERVER = "tile.openstreetmap.org";
-    //Je trouve ce nom moche hhh. et malheureusement j'ai pas eu la version modifiée.
-    private final static String CYCLO_OSM = "CyclOSM           ";
+    private final static String CYCLO_OSM = "CyclOSM";
     private final static String CYCLO_OSM_SERVER = "a.tile-cyclosm.openstreetmap.fr/cyclosm";
 
     public SwitchBackgroundManager(AnnotatedMapManager annotatedMapManager, Path cacheBasePath) {
@@ -42,6 +42,7 @@ public final class SwitchBackgroundManager {
         osmButton.setSelected(true);
         cyclosmButton = new RadioButton(CYCLO_OSM);
 
+
         ToggleGroup backgroundButtons = new ToggleGroup();
         osmButton.setToggleGroup(backgroundButtons);
         cyclosmButton.setToggleGroup(backgroundButtons);
@@ -49,6 +50,7 @@ public final class SwitchBackgroundManager {
         tilePane = new TilePane(label, osmButton, cyclosmButton);
         tilePane.setOrientation(Orientation.VERTICAL);
         tilePane.setBackground(Background.fill(Color.MINTCREAM));
+
 
         //En testant, je me suis rendue compte qu'il n'y avait que la height qui faisait n'importe
         //quoi niveau taille d'origine mais la width non. Sauf que je sais pas prk la height n'est pas
@@ -60,7 +62,7 @@ public final class SwitchBackgroundManager {
 
         pane = new Pane(tilePane);
         pane.setPickOnBounds(false);
-
+        tilePane.setTileAlignment(Pos.CENTER_LEFT);
         bindings();
         events();
     }
